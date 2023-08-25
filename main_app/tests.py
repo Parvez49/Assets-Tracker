5fd23fcf058cc2b795234b5746063c77206e2607
Parvez Hossen <parvezhossen81@gmail.com>
2023-08-25 16:13:09 +0600

    test case added for Company model

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Company
+
+class CompanyModelTestCase(TestCase):
+    def setUp(self):
+        Company.objects.create(name='company', registration_no='COMP123')
+
+    def test_company_name(self):
+        company = Company.objects.get(registration_no='COMP123')
+        self.assertEqual(company.name, 'company')
+
+    def test_company_registration_no(self):
+        company = Company.objects.get(name='company')
+        self.assertEqual(company.registration_no, 'COMP123')
+
+    def test_company_str_representation(self):
+        company = Company.objects.get(name='company')
+        self.assertEqual(str(company), 'company')
